0b393036503aa0c1415e16a05b441dce770c06e8
Melanie McCall <melanie@hiddenlevers.com>
2020-02-29 10:18:28 -0500

    first commit everything

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+                $(".carousel").carousel("pause");
+                $("#carouselButton").children("i").removeClass("fa-pause");
+                $("#carouselButton").children("i").addClass("fa-play");
+            } else {
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause"); 
+            }
+        });
+
+//Moving this inside of your document ready fxn 
+        $('#loginButton').click(function(){
+            $('#loginModal').modal("show")
+        })
+        $('#reserveButton').click(function(){
+            $('#reserveModal').modal("show");
+        })
+
+    });
